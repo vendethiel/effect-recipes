387a8e9566334246ff67eb59d743b7866ef3ffea
vendethiel <vendethiel@hotmail.fr>
2025-11-29 00:32:11 +0100

    remove config that's now done in layerConfig

--- src/Db.ts
+++ src/Db.ts
@@ -1,28 +1,7 @@
 import * as PgKysely from "@effect/sql-kysely/Pg";
 import { PgClient } from "@effect/sql-pg";
-import { Config, Context, Effect, Layer, Redacted } from "effect";
+import { Config, Context, Layer, Redacted } from "effect";
 import type { RecipeTable } from "./Recipes/Table";
-
-export class DbConfig extends Effect.Service<DbConfig>()("DbConfig", {
-  effect: Effect.gen(function* () {
-    return {
-      host: yield* Config.string("DB_HOST").pipe(
-        Config.withDefault("localhost"),
-      ),
-      port: yield* Config.integer("DB_PORT").pipe(Config.withDefault(5432)),
-      database: yield* Config.string("DB_NAME").pipe(
-        Config.withDefault("recipes"),
-      ),
-      username: yield* Config.string("DB_USERNAME").pipe(
-        Config.withDefault("postgres"),
-      ),
-      password: yield* Config.redacted("DB_PASSWORD").pipe(
-        Config.withDefault(Redacted.make("postgres")),
-      ),
-      ssl: yield* Config.boolean("DB_SSL").pipe(Config.withDefault(false)),
-    };
-  }),
-}) {}
 
 export const PgLive = PgClient.layerConfig({
   host: Config.string("DB_HOST").pipe(
