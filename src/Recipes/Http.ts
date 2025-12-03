9c9b3a005ab85ed5f3ce0ec3267e66002d934459
vendethiel <vendethiel@hotmail.fr>
2025-12-03 09:57:03 +0100

    Unwrap Http's Effect.gen while we do not yield other services

--- src/Recipes/Http.ts
+++ src/Recipes/Http.ts
@@ -1,41 +1,32 @@
 import { HttpApiBuilder } from "@effect/platform";
 import { Effect, Layer } from "effect";
 import { Api } from "src/Api";
-import { RecipeNotFound, RecipeCreationError } from "src/Recipes/Model";
+import { RecipeCreationError, RecipeNotFound } from "src/Recipes/Model";
 import { RecipeRepository } from "./Repository";
 
 export const HttpRecipesLive = HttpApiBuilder.group(
   Api,
   "recipes",
   (handlers) =>
-    Effect.gen(function* () {
-      return handlers
-        .handle(
-          "list",
-          () => RecipeRepository.list().pipe(Effect.orDie))
-        .handle(
-          "get",
-          ({ path: { id } }) => RecipeRepository.get(id).pipe(
-            Effect.catchTag(
-              "NoSuchElementException",
-              () => RecipeNotFound.make({ id })
+    Effect.succeed(
+      handlers
+        .handle("list", () => RecipeRepository.list().pipe(Effect.orDie))
+        .handle("get", ({ path: { id } }) =>
+          RecipeRepository.get(id).pipe(
+            Effect.catchTag("NoSuchElementException", () =>
+              RecipeNotFound.make({ id }),
             ),
             Effect.orDie,
           ),
         )
-        .handle(
-          "create",
-          ({ payload }) =>
-            // TODO auth
-            RecipeRepository.create(payload).pipe(
-              Effect.catchTag(
-                "NoSuchElementException",
-                () => RecipeCreationError.make()
-              ),
-              Effect.orDie
-            )
-        );
-    }),
-).pipe(Layer.provide([
-  RecipeRepository.Default
-]));
+        .handle("create", ({ payload }) =>
+          // TODO auth
+          RecipeRepository.create(payload).pipe(
+            Effect.catchTag("NoSuchElementException", () =>
+              RecipeCreationError.make(),
+            ),
+            Effect.orDie,
+          ),
+        ),
+    ),
+).pipe(Layer.provide([RecipeRepository.Default]));
